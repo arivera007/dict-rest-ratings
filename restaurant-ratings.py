11fe1bd102b1337d4873cbfa82e5e552dc82204d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 17:55:14 -0800

    working with user options: 1 2 or 3

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -29,12 +29,64 @@ def get_ratings_from_disk():
 
     return ratings
 
+
+def view_ratings():
+
+    for item, rating in sorted(main_ratings.items()):
+        print "%s is rated at %d." % (item, rating)
+
+
+def add_rating():
+
+    user_restaurant_name = raw_input("Please enter a restaurant name: ")
+    user_restaurant_rating = int(raw_input("Please enter the restaurant score: "))
+    main_ratings[user_restaurant_name] = user_restaurant_rating
+    view_ratings()
+
+
+def execute_user_choice(choice):
+    while choice != "3":
+        if choice == "1":
+            view_ratings()
+        elif choice == "2":
+            add_rating()
+        else:
+            get_user_choice()
+        break
+
 main_ratings = {}
 
-user_restaurant_name = raw_input("Please enter a restaurant name: ")
-user_restaurant_rating = int(raw_input("Please enter the restaurant score: "))
 main_ratings = get_ratings_from_disk()
-main_ratings[user_restaurant_name] = user_restaurant_rating
 
-for item, rating in sorted(main_ratings.items()):
-    print "%s is rated at %d." % (item, rating)
+
+def get_user_choice():
+    print "Would you like to: "
+    print "1. see all ratings in alpha order"
+    print "2. add a new restaurant"
+    print "3. Quit"
+    execute_user_choice(raw_input("Enter 1, 2, 3: "))
+
+#get_user_choice()
+
+# while user_input != "3":
+
+#     if user_input == "1":
+#         view_ratings()
+#     elif user_input == "2":
+#         add_rating()
+#     else:
+#         user_input = '3'
+
+while True:
+    print "Would you like to: "
+    print "1. see all ratings in alpha order"
+    print "2. add a new restaurant"
+    print "3. Quit"
+    choice = raw_input("Enter 1, 2, 3: ")
+    if choice == "1":
+        view_ratings()
+    elif choice == "2":
+        add_rating()
+    else:
+        # get_user_choice()
+        break
